1a7a305e048efdb6f16ba151a3057b708c24bd75
whiteHeartblanc <45506990+whiteHeartblanc@users.noreply.github.com>
2021-01-15 15:42:45 +0800

    main fix pls

--- src/app/ExpressionApp.java
+++ src/app/ExpressionApp.java
@@ -43,17 +43,19 @@ public class ExpressionApp {
 			//syntax error handling
 			parser.removeErrorListeners();
 			//parser.setErrorHandler(new DefaultErrorStrategy());
-			parser.setErrorHandler(new DefaultErrorStrategy() {
+			/*parser.setErrorHandler(new DefaultErrorStrategy() {
 				 @Override
 			        public void recover(Parser recognizer, RecognitionException e) {
-			            if (lastErrorIndex==recognizer.getInputStream().index() && lastErrorStates != null && lastErrorStates.contains(recognizer.getState())) {
+			          //  if (lastErrorIndex==recognizer.getInputStream().index() && lastErrorStates != null && lastErrorStates.contains(recognizer.getState())) {
 			            	recognizer.consume();
-			            }
+			           // }
 			            if(lastErrorStates ==null)
 			            	lastErrorStates = new IntervalSet();
 			            lastErrorStates.add(recognizer.getState());
 			            IntervalSet followSet = getErrorRecoverySet(recognizer);
-			            consumeUntil(recognizer, followSet);
+			          consumeUntil(recognizer, followSet);
+			            int currenttoken= ((Token) e.getOffendingToken()).getTokenIndex();
+			            System.out.println(followSet.getIntervals() + " " + ((Parser)recognizer).getTokenStream().get(currenttoken).getText() + " "+ recognizer.getInputStream().index());
 			        }
 				/*
 				 public Token recoverInline(Parser recognizer)
@@ -68,7 +70,36 @@ public class ExpressionApp {
 				            throw new ParseCancellationException(e);
 				        }*/
 				
+		//	});
+			/*
+			parser.setErrorHandler(new DefaultErrorStrategy() {
+				@Override
+		        public void recover(Parser recognizer, RecognitionException e) {
+//			        System.out.println("recover in "+recognizer.getRuleInvocationStack()+
+//			                           " index="+recognizer.getInputStream().index()+
+//			                           ", lastErrorIndex="+
+//			                           lastErrorIndex+
+//			                           ", states="+lastErrorStates);
+			        if ( lastErrorIndex==recognizer.getInputStream().index() &&
+			            lastErrorStates != null &&
+			            lastErrorStates.contains(recognizer.getState()) ) {
+			            // uh oh, another error at same token index and previously-visited
+			            // state in ATN; must be a case where LT(1) is in the recovery
+			            // token set so nothing got consumed. Consume a single token
+			            // at least to prevent an infinite loop; this is a failsafe.
+//			            System.err.println("seen error condition before index="+
+//			                               lastErrorIndex+", states="+lastErrorStates);
+//			            System.err.println("FAILSAFE consumes "+recognizer.getTokenNames()[recognizer.getInputStream().LA(1)]);
+			            recognizer.consume();
+			        }
+			        lastErrorIndex = recognizer.getInputStream().index();
+			        if ( lastErrorStates==null ) lastErrorStates = new IntervalSet();
+			        lastErrorStates.add(recognizer.getState());
+			        IntervalSet followSet = getErrorRecoverySet(recognizer);
+			        consumeUntil(recognizer, followSet);
+			    }
 			});
+			*/
 			//parser.addErrorListener(new SyntaxErrorListener());
 			parser.addErrorListener(new MyErrorSyntaxListener());
 		}catch(IOException e) {
